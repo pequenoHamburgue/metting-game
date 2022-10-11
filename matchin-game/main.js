fabec39d69dca4620d8aca52796c55b271b401a2
pequeno <christopherwiilan100@gmail.com>
2022-10-11 18:21:35 -0300

    create Player name

--- matchin-game/main.js
+++ matchin-game/main.js
@@ -5,19 +5,24 @@ import './src/styles/settings/elements/base.css';
 import BoardGame from "./src/objects/BoardGame";
 
 import CardGame from "./src/components/CardGame";
-import NamePlayer from "./src/components/NamePlayers";
+import PlayerName from "./src/components/PlayerName";
 
 const $root = document.querySelector("#root");
-const $htmlBoardGame = BoardGame(6);
-const $htmlNamePlayer = NamePlayer();
+//const $htmlBoardGame = BoardGame();
+const $htmlNamePlayer = PlayerName();
 
 //1- creat 6 cards
 //2- creat components playes
 //3- creat a versing card
 
 //$root.render($htmlCardGame)
-$root.insertAdjacentHTML("afterbegin", $htmlNamePlayer);
-$root.insertAdjacentHTML("beforeend", $htmlBoardGame);
+
+$root.insertAdjacentHTML(
+    "beforeend",`
+    ${PlayerName('Player1')},
+    ${PlayerName('Player2')},   
+    ${BoardGame(6)}`
+);
 
 
 
---
+++ matchin-game/src/components/PlayerName/index.js
@@ -0,0 +1,9 @@
+import "./style.css";
+function PlayerName(name){
+    return `
+    <p class="name-player">
+    ${name}
+    </p>
+    `;
+}
+export default PlayerName;
