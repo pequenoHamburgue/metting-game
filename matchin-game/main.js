a04f945794279beea4f3cbed3ebc22debf1bf4e5
pequeno <christopherwiilan100@gmail.com>
2022-10-11 10:31:37 -0300

    tentativa do desafio

--- matchin-game/main.js
+++ matchin-game/main.js
@@ -2,10 +2,26 @@ import './src/styles/settings/colors.css';
 import './src/styles/generic/reset.css'
 import './src/styles/settings/elements/base.css';
 
+
 import CardGame from "./src/components/CardGame";
+import NamePlayer from "./src/components/NamePlayers";
 
 const $root = document.querySelector("#root");
 const $htmlCardGame = CardGame();
+const $htmlNamePlayer = NamePlayer();
 
-console.log($htmlCardGame)
+//1- creat 6 cards
+//2- creat components playes
+//3- creat a versing card
+
+//$root.render($htmlCardGame)
+$root.insertAdjacentHTML("afterbegin", $htmlNamePlayer);
 $root.insertAdjacentHTML("beforeend", $htmlCardGame);
+$root.insertAdjacentHTML("beforeend", $htmlCardGame);
+$root.insertAdjacentHTML("beforeend", $htmlCardGame);
+$root.insertAdjacentHTML("beforeend", $htmlCardGame);
+$root.insertAdjacentHTML("beforeend", $htmlCardGame);
+$root.insertAdjacentHTML("beforeend", $htmlCardGame);
+
+
+
---
+++ matchin-game/src/components/NamePlayers/index.js
@@ -0,0 +1,10 @@
+import "./style.css";
+function NamePlayer(number) {
+    return `
+    <div class="name-player">
+    Player 1
+    </div>
+    `
+}
+
+export default NamePlayer;
