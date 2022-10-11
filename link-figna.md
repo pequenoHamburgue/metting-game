3dda426a59eb813ef4f42268e5605dd4bfba5f5f
pequeno <christopherwiilan100@gmail.com>
2022-10-11 08:53:56 -0300

    fist commit

---
+++ link-figna.md
@@ -0,0 +1,2 @@
+https://www.figma.com/file/k8GPJJNlqcjXvnF6Nmk2XL/Matching-game?node-id=1%3A129
+
---
+++ matchin-game/src/components/CardGame/index.js
@@ -0,0 +1,10 @@
+import "./style.css";
+function CardGame() {
+    return `
+    <article class="card-game"> 
+        <img src="images/alura-pixel 1.png" alt="logo da Alura"
+    </article>`
+    
+}
+
+export default CardGame;
